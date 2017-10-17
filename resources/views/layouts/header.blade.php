7e8bdba65271a66d655bb09a2b93e1ea917bbc3c
Egbert-Jan <ejterpstra@icloud.com>
2017-10-17 08:34:17 +0200

    In de header zijn de links goed gemaakt

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -10,10 +10,10 @@
         <nav style="background-color:#eec86e;">
             <div class="nav-wrapper">
                 <ul id="nav-mobile" class="right">
-                    @if($_SERVER['REQUEST_URI'] == "/kruipapp/public/scores")
+                    @if($_SERVER['REQUEST_URI'] == "/scores")
                         <li><a href="{{url('/scores/create')}}">Nieuwe game</a></li>
                         <li><a href="{{url('/home')}}">Mijn info</a></li>
-                    @elseif($_SERVER['REQUEST_URI'] == "/kruipapp/public/scores/create")
+                    @elseif($_SERVER['REQUEST_URI'] == "/create")
                         <li><a href="{{url('/scores')}}">Scores</a></li>
                         <li><a href="{{url('/home')}}">Mijn info</a></li>
                     @endif
