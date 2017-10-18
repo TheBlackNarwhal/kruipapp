2fc9c3a114dfa3f4e69593cec069b00f610875f7
Egbert-Jan <ejterpstra@icloud.com>
2017-10-18 16:44:49 +0200

    Page titels aangepast maar miss nog niet helemaal goed

--- resources/views/scores/index.blade.php
+++ resources/views/scores/index.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <title>Document</title>
+    <title>Scores</title>
 
     <style>
           
